179c45f1fe0b07e28ab865b8e0a163971ea5ac71
AZERFON\gsamadova <fraugunsche@gmail.com>
2017-02-23 18:14:59 +0400

    no message

--- src/com/guny/hiber/example/dto/UserDetails.java
+++ src/com/guny/hiber/example/dto/UserDetails.java
@@ -1,58 +1,92 @@
 package com.guny.hiber.example.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name="USER_DETAILS")
 public class UserDetails {
 	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="USER_ID")
 	private int userId;
-	@Transient
+	@Column(name="USER_NAME")
 	private String userName;
-	@Temporal(TemporalType.DATE)
+	@Column(name="JOINED_DATE")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date joinedDate;
-	private String address;
 	@Lob
 	private String description;
+	@ElementCollection
+	@JoinTable(name="USER_ADDRESS", joinColumns=@JoinColumn(name="USER_ID"))
+	/*@GenericGenerator(name="sequence-gen",strategy="sequence")
+	@CollectionId(columns={@Column(name="ADDRESS_ID")}, generator = "sequence-gen", type = @Type(type = "long"))*/
+	private Collection<Address> listOfAddresses = new ArrayList<Address>();
+	
+	/*
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="street", column=@Column(name="HOME_STREET")),
+		@AttributeOverride(name="city", column=@Column(name="HOME_CITY")),
+		@AttributeOverride(name="state", column=@Column(name="HOME_STATE")),
+		@AttributeOverride(name="pincode", column=@Column(name="HOME_PIN"))
+	})
+	private Address homeAddress;
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="street", column=@Column(name="OFFICE_STREET")),
+		@AttributeOverride(name="city", column=@Column(name="OFFICE_CITY")),
+		@AttributeOverride(name="state", column=@Column(name="OFFICE_STATE")),
+		@AttributeOverride(name="pincode", column=@Column(name="OFFICE_PIN"))
+	})
+	private Address officeAddress;
+*/
+	public UserDetails() {
+		super();
+	}
+	
+	public UserDetails(String userName, Date joinedDate, String description) {
+		super();
+		this.userName = userName;
+		this.joinedDate = joinedDate;
+		this.description = description;
+	}
 
-	@Id @GeneratedValue
-	@Column(name="USER_ID")
 	public int getUserId() {
 		return userId;
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-
-	@Column(name="USER_NAME")
 	public String getUserName() {
 		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
 	}
 	public String getDescription() {
 		return description;
@@ -60,4 +94,13 @@ public class UserDetails {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
+	}
+
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+	
 }
--- src/com/guny/hibernate/HibernateTest.java
+++ src/com/guny/hibernate/HibernateTest.java
@@ -4,40 +4,29 @@ import java.util.Date;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
+import com.guny.hiber.example.dto.Address;
 import com.guny.hiber.example.dto.UserDetails;
 
 public class HibernateTest {
 	
 	private static SessionFactory factory;
-	private static ServiceRegistry registry;
-
+	private static Session session;
+	
 	public static void main(String[] args) {
 
-		UserDetails user = new UserDetails();
-		//user.setUserId(1);
-		user.setUserName("1 User");
-		user.setAddress("1 address");
-		user.setJoinedDate(new Date());
-		user.setDescription("1 Description");
+		UserDetails user = new UserDetails("User1", new Date(), "Description1");
+		Address home = new Address("home_street1", "home_city1", "home_state1", "home_pin1");
+		//user.setHomeAddress(home);
+		Address office = new Address("office_street1", "office_city1", "office_state1", "office_pin1");
+		//user.setOfficeAddress(office);
+		user.getListOfAddresses().add(home);
+		user.getListOfAddresses().add(office);
 		
-		UserDetails user2 = new UserDetails();
-		user2.setUserName("2 user");
-				
-		Configuration conf = new Configuration().configure();
-		registry = new StandardServiceRegistryBuilder().
-				applySettings(conf.getProperties()).build();
-		factory = conf.addPackage("com.guny.hiber.example.dto").
-				addAnnotatedClass(UserDetails.class).
-				buildSessionFactory(registry);
-		//factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
+		factory = new Configuration().configure().buildSessionFactory();
+		session = factory.openSession();
 		session.beginTransaction();
 		session.save(user);
-		session.save(user2);
 		session.getTransaction().commit();
 		session.close();
 		
@@ -46,6 +35,8 @@ public class HibernateTest {
 		session.beginTransaction();
 		user = (UserDetails) session.get(UserDetails.class, 1);
 		System.out.println("User Name retrieved is " + user.getUserName());
+		user.getListOfAddresses();
+		session.close();
 	}
 
 }
